73219d76ac3f3ced6d3b1586ad7a787de3da2802
krnzt <krnzt@pm.me>
2026-04-09 19:54:25 -0400

    feat(08-01): update roz-cli build.rs to compile embodiment.proto
    - Add embodiment.proto alongside agent.proto in compile_protos call
    - Enables EmbodimentServiceClient codegen for CLI commands

--- crates/roz-cli/build.rs
+++ crates/roz-cli/build.rs
@@ -2,6 +2,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
         .build_client(true)
-        .compile_protos(&["../../proto/roz/v1/agent.proto"], &["../../proto"])?;
+        .compile_protos(
+            &[
+                "../../proto/roz/v1/agent.proto",
+                "../../proto/roz/v1/embodiment.proto",
+            ],
+            &["../../proto"],
+        )?;
     Ok(())
 }
